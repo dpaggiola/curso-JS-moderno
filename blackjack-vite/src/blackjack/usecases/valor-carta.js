3127cd94c4f7b1c0a8aedec26718ca70fb658483
Daniel Paggiola <dpaggiola@gmail.com>
2024-07-30 13:49:09 -0300

    Section 8 done

---
+++ blackjack-vite/src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,10 @@
+/**
+ * Esta función devuelve el valor de una carta
+ * @param {String} carta
+ * @returns { Number } retorna el valor de una carta
+ */
+export const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
