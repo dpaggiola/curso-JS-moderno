6f0359a5140cc45a8d99ae53ac55c37dda582e60
Daniel Paggiola <dpaggiola@gmail.com>
2024-07-31 18:44:54 -0300

    Class 112

---
+++ todo-app/src/store/todo.store.js
@@ -0,0 +1,74 @@
+import { Todo } from '../todos/models/todo.model';
+
+const Filters = {
+  All: 'All',
+  Completed: 'Completed',
+  Pending: 'Pending'
+}
+
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del infinito'),
+        new Todo('Piedra del tiempo'),
+    ],
+    filter: Filters.All,
+}
+
+
+const initStore = () => {
+  console.log(state);
+  console.log('InitStore');
+}
+
+const loadStore = () => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+}
+
+export default {
+  addTodo, 
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
+}
