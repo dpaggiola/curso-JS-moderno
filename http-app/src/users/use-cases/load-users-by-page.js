580e09806def1d64a4dc524e763d26ff55d4a6cf
Daniel Paggiola <dpaggiola@gmail.com>
2024-08-09 13:19:41 -0300

    Fix table in class 166

--- http-app/src/users/use-cases/load-users-by-page.js
+++ http-app/src/users/use-cases/load-users-by-page.js
@@ -11,8 +11,8 @@ export const loadUsersByPage = async(page = 1) => {
   const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
   const res = await fetch(url);
   const data = await res.json();
-
-  const users = data.map(localhostUserToModel);
+  
+  const users = data.data.map(localhostUserToModel);
 
   return users;
 }
--- http-app/src/users/users-app.js
+++ http-app/src/users/users-app.js
@@ -1,7 +1,10 @@
 import usersStore from "./store/users-store";
 import {renderTable} from "./presentation/render-table/render-table";
 
-
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
 export const UsersApp = async(element) => {
     element.innerHTML = 'Loading...';
     await usersStore.loadNextPage();
